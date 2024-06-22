42c3822f00939cb9be719e97832cbf03e179505f
diogofbrito <diogobrito74@gmail.com>
2024-06-22 01:04:18 +0100

    feat: add .ENV file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const { VITE_API_BASE_URL } = import.meta.env;
+
 export const api = axios.create({
-	baseURL: 'http://localhost:3002',
+	baseURL: VITE_API_BASE_URL,
 	headers: {
 		'Content-Type': 'application/json',
 	},
